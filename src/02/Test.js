4e0103e6afb7d47a6f94e0c4d2e5abc91ce2f6da
jg <jjtjs159@gmail.com>
2020-09-16 07:40:03 +0900

    2-6 javascript

---
+++ src/02/Test.js
@@ -0,0 +1,15 @@
+// alert('hello');
+
+// 모달 창은 떠 있는 동안 스크립스 실행이 일시 중단되며, 사용자가 창을 닫기 전까지 다른 페이지와 상호작용 X
+
+// 제목, 초기값을 가진 입력필드
+// prompt('title', 'initial');
+
+// let age = prompt('your age..', 100);
+
+// alert(`my age - ${age}`);
+
+// confirm - true, cancel - false
+// let isBoss = confirm('text...');
+
+// alert(isBoss);
