a74f9cea1f9d6fb1c453bf2ac82cc73bfe16a938
jg <jjtjs159@gmail.com>
2020-09-27 11:16:23 +0900

    2-10 javascript

---
+++ src/03/Condition.js
@@ -0,0 +1,21 @@
+// let year = prompt('ECMAScript-2015 명세는 몇 년도에 출판되었을까요?', '');
+
+// if (year == 2015) alert( '정답입니다!' );
+
+// let cond = year == 2015;
+
+// if (cond) {
+//     alert('test')
+// } else {
+//     alert('else');
+// }
+
+// let result = cond ? '22' : '33';
+// alert(result);
+
+let a = 2;
+let b = 1;
+
+let result = (a + b < 4) ? '미만' : '이상';
+
+alert(result);
